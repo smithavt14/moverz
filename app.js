391e4ab035c992d947331bef00dcae5a81421bb8
Alex Smith <smithavt14@gmail.com>
2019-10-26 17:18:36 +0800

    Uprgrade the plugin version and add autologin: true to fix login issues

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ App({
   onLaunch: function () {
     wx.BaaS = requirePlugin('sdkPlugin')
     //让插件帮助完成登录、支付等功能
-    wx.BaaS.wxExtend(wx.login,
-      wx.getUserInfo,
-      wx.requestPayment)
+    wx.BaaS.wxExtend(wx.login, wx.getUserInfo, wx.requestPayment)
 
-    wx.BaaS.init('5ad04de13f0715885f66')
+    wx.BaaS.init('5ad04de13f0715885f66', { autoLogin: true })
   },
 })
